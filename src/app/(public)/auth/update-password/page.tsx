b41e0c04358e8edc83155e4c81a7cb4e53c7a1c7
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-16 12:14:31 +0500

    Add UpdatePasswordForm component and implement UpdatePasswordPage for password updating functionality

--- src/app/(public)/auth/update-password/page.tsx
+++ src/app/(public)/auth/update-password/page.tsx
@@ -1,3 +1,22 @@
+import { UpdatePasswordForm } from "~/components/forms/update-password-form";
+import { domine } from "~/lib/fonts";
+import { cn } from "~/lib/utils";
+
 export default function UpdatePasswordPage() {
-  return <></>;
+  return (
+    <div className={cn("flex-1 self-center space-y-8 md:p-4")}>
+      <div className={cn("space-y-2 text-center")}>
+        <h2
+          className={cn("text-black/75 text-3xl font-bold", domine.className)}
+        >
+          Update Password
+        </h2>
+        <p className={"text-muted-foreground text-base font-medium"}>
+          Enter your new password to update your account. Make sure to choose a
+          strong password that you haven't used before.
+        </p>
+      </div>
+      <UpdatePasswordForm />
+    </div>
+  );
 }
---
+++ src/components/forms/update-password-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+
+import axios, { AxiosError } from "axios";
+import { Loader2Icon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import zod from "zod";
+
+import { toast } from "sonner";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { routes } from "~/lib/routes";
+import { OtpType } from "~/lib/types";
+import { cn } from "~/lib/utils";
+
+const UpdatePasswordFormSchema = zod.object({
+  password: zod.string({
+    message: "Password must be string",
+  }),
+});
+
+async function updatePassword({
+  password,
+}: zod.infer<typeof UpdatePasswordFormSchema>) {
+  const response = await axios.post(routes.api.auth.updatePassword.url(), {
+    password,
+  });
+
+  return response.data;
+}
+
+export function UpdatePasswordForm() {
+  const router = useRouter();
+
+  const form = useForm<zod.infer<typeof UpdatePasswordFormSchema>>({
+    resolver: zodResolver(UpdatePasswordFormSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  const updatePasswordMutation = useMutation({
+    mutationFn: updatePassword,
+    onSuccess: ({ info }) => {
+      toast.success(info.message);
+
+      sessionStorage.removeItem("token");
+
+      router.push(routes.app.auth.signIn.url());
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.info.message);
+      }
+    },
+    onSettled: () => {
+      form.reset();
+    },
+  });
+
+  const onSubmit = (data: zod.infer<typeof UpdatePasswordFormSchema>) => {
+    updatePasswordMutation.mutate(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={cn("space-y-6")}>
+        <div className={cn("flex gap-2 items-start")}>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className={cn("flex-1")}>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="********" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className={cn("space-x-4")}>
+          <Button
+            variant="default"
+            size="lg"
+            className={cn("w-full")}
+            type="submit"
+            disabled={updatePasswordMutation.isPending}
+          >
+            {updatePasswordMutation.isPending && (
+              <Loader2Icon className={cn("animate-spin")} />
+            )}
+            <span>Update Password</span>
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
