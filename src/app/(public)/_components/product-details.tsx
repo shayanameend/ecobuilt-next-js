85b712beb8e284bf19fdb61e4e35cbbc594cd86f
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-04-02 12:53:50 +0500

    Refactor ProductDetails component for improved structure and styling; update image dimensions, enhance vendor name display, and adjust price formatting

--- src/app/(public)/_components/product-details.tsx
+++ src/app/(public)/_components/product-details.tsx
@@ -17,61 +17,77 @@ import { routes } from "~/lib/routes";
 import { cn, formatPrice } from "~/lib/utils";
 import { $cart } from "~/stores/cart";
 
-interface ProductDetails {
+interface ProductDetailsProps {
   product: PublicProductType & {
     category: PublicCategoryType;
     vendor: VendorProfileType;
   };
 }
 
-export function ProductDetails({ product }: Readonly<ProductDetails>) {
+export function ProductDetails({ product }: Readonly<ProductDetailsProps>) {
   const cart = useStore($cart);
 
   return (
-    <Link href={routes.app.public.products.url(product.id)}>
-      <Card className={cn("p-0 gap-0")}>
-        <CardContent className={cn("p-0")}>
+    <Card className={cn("p-0 gap-0")}>
+      <Link href={routes.app.public.products.url(product.id)}>
+        <CardContent className={cn("py-5 px-10")}>
           <Image
             src={`${process.env.NEXT_PUBLIC_FILE_URL}/${product.pictureIds[0]}`}
             alt={product.name}
-            width={180}
-            height={180}
-            sizes="(max-width: 640px) 120px, (max-width: 1024px) 150px, 180px"
-            className={cn("h-48 w-full object-cover rounded-t-xl")}
+            width={240}
+            height={240}
+            className={cn("h-60 w-full object-contain rounded-t-xl")}
           />
         </CardContent>
-        <CardFooter className={cn("p-4 flex-col items-stretch gap-2")}>
-          <div>
-            <h3 className={cn("")}>{product.name}</h3>
+      </Link>
+      <CardFooter className={cn("p-4 flex-col items-stretch gap-2 bg-gray-50")}>
+        <div>
+          <h4 className={cn("text-[10px] uppercase")}>{product.vendor.name}</h4>
+          <h3 className={cn("text-[16px]")}>{product.name}</h3>
+        </div>
+        <div className={cn("flex justify-between items-center")}>
+          <div className={cn("-space-y-1")}>
+            <p className={cn("space-x-1 text-lg font-medium")}>
+              <span>
+                {product.salePrice
+                  ? formatPrice(product.salePrice)
+                  : formatPrice(product.price)}
+              </span>
+              {product.salePrice && (
+                <span
+                  className={cn("text-muted-foreground text-sm line-through")}
+                >
+                  {formatPrice(product.price)}
+                </span>
+              )}
+            </p>
+            <p className={cn("text-[10px] text-muted-foreground")}>Excl. VAT</p>
           </div>
-          <div className={cn("flex justify-between items-center")}>
-            <p className={cn("")}>{formatPrice(product.price)}</p>
-            <Button
-              variant="default"
-              size="default"
-              onClick={() => {
-                if (cart.items.find((item) => item.id === product.id)) {
-                  $cart.set({
-                    ...cart,
-                    items: cart.items.map((item) =>
-                      item.id === product.id
-                        ? { ...item, quantity: item.quantity + 1 }
-                        : item,
-                    ),
-                  });
-                } else {
-                  $cart.set({
-                    ...cart,
-                    items: [...cart.items, { ...product, quantity: 1 }],
-                  });
-                }
-              }}
-            >
-              Purchase
-            </Button>
-          </div>
-        </CardFooter>
-      </Card>
-    </Link>
+          <Button
+            variant="default"
+            size="default"
+            onClick={() => {
+              if (cart.items.find((item) => item.id === product.id)) {
+                $cart.set({
+                  ...cart,
+                  items: cart.items.map((item) =>
+                    item.id === product.id
+                      ? { ...item, quantity: item.quantity + 1 }
+                      : item,
+                  ),
+                });
+              } else {
+                $cart.set({
+                  ...cart,
+                  items: [...cart.items, { ...product, quantity: 1 }],
+                });
+              }
+            }}
+          >
+            Purchase
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
