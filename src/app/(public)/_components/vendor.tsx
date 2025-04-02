751742e8e3c22cdf7279090e08845b46a3d022d3
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-04-02 12:00:07 +0500

    Refactor Vendor component for improved styling and layout; adjust image size and add font styling for vendor name

--- src/app/(public)/_components/vendor.tsx
+++ src/app/(public)/_components/vendor.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Card, CardContent, CardFooter } from "~/components/ui/card";
+import { domine } from "~/lib/fonts";
 import { routes } from "~/lib/routes";
 import { cn } from "~/lib/utils";
 
@@ -17,19 +18,20 @@ export function Vendor({ vendor }: Readonly<VendorProps>) {
   return (
     <Link href={routes.app.public.vendors.url(vendor.id)}>
       <Card className={cn("p-0 gap-0")}>
-        <CardContent className={cn("p-0")}>
+        <CardContent className={cn("py-5 px-10")}>
           <Image
             src={`${process.env.NEXT_PUBLIC_FILE_URL}/${vendor.pictureId}`}
             alt={vendor.name}
-            width={180}
-            height={180}
-            sizes="(max-width: 640px) 120px, (max-width: 1024px) 150px, 180px"
-            className={cn("h-48 w-full object-cover rounded-t-xl")}
+            width={64}
+            height={64}
+            className={cn("h-16 w-full object-contain rounded-t-xl")}
           />
         </CardContent>
         <CardFooter className={cn("p-4 flex-col items-stretch gap-2")}>
           <div>
-            <h3 className={cn("")}>{vendor.name}</h3>
+            <h3 className={cn("text-2xl font-bold", domine.className)}>
+              {vendor.name}
+            </h3>
           </div>
           <div className={cn("flex justify-between items-center")}>
             <p className={cn("")}>{vendor.description}</p>
