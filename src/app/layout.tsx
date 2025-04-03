e9a09f3d85e97211e100fffc59e0308a6387a0e7
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-04-03 21:10:07 +0500

    Add Tidio chat script to RootLayout for enhanced customer support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from "react";
 
+import Script from "next/script";
+
 import { RootProvider } from "./_components/root-provider";
 
 import "~/styles/globals.css";
@@ -9,6 +11,11 @@ export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
     <html lang="en">
       <body>
         <RootProvider>{children}</RootProvider>
+        <Script
+          src="//code.tidio.co/kqf9guvqo5gtjv8urt7mcen7j2hqtcmc.js"
+          strategy="afterInteractive" // Or "lazyOnload"
+          async
+        />
       </body>
     </html>
   );
